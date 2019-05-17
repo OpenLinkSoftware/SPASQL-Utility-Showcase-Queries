bbc46eceecc8b7973175c86dc0f2c32b5d399b7b
OpenLink Software <github@openlinksw.com>
2019-05-17 09:57:04 -0400

    Capitalization of Procedure Names

--- northwind-csv-spasql-demo.sql
+++ northwind-csv-spasql-demo.sql
@@ -25,78 +25,78 @@
 -- ATTACH CATEGORIES TABLE
 
 DROP TABLE csv.northwind.categories;
-attach_from_csv ('csv.northwind.categories', 'file:../vad/categories.csv', ',', '\n', null, 1, vector(1));
+ATTACH_FROM_CSV ('csv.northwind.categories', 'file:../vad/categories.csv', ',', '\n', null, 1, vector(1));
 SELECT TOP 5 * FROM csv.northwind.categories;
 
 --ATTACH CUSTOMERS TABLE
 
 DROP TABLE csv.northwind.customers;
-attach_from_csv ('csv.northwind.customers', 'file:../vad/customers.csv', ',', '\n', null, 1, vector(1));
+ATTACH_FROM_CSV ('csv.northwind.customers', 'file:../vad/customers.csv', ',', '\n', null, 1, vector(1));
 SELECT TOP 5 * FROM csv.northwind.customers;
 
 
 --ATTACH EMPLOYEE TERRITORY TABLE
 
 DROP TABLE csv.northwind.employee_territories;
-attach_from_csv ('csv.northwind.employee_territories', 'file:../vad/employee_territories.csv', ',', '\n', null, 1, vector(1));
+ATTACH_FROM_CSV ('csv.northwind.employee_territories', 'file:../vad/employee_territories.csv', ',', '\n', null, 1, vector(1));
 SELECT TOP 5 * FROM csv.northwind.employee_territories;
 
 
 --ATTACH EMPLOYEES TABLE
 
 DROP TABLE csv.northwind.employees;
-attach_from_csv ('csv.northwind.employees', 'file:../vad/employees.csv', ',', '\n', null, 1, vector(1));
+ATTACH_FROM_CSV ('csv.northwind.employees', 'file:../vad/employees.csv', ',', '\n', null, 1, vector(1));
 SELECT TOP 5 * FROM csv.northwind.employees;
 
 
 --ATTACH ORDER DETAILS TABLE
 
 DROP TABLE csv.northwind.order_details;
-attach_from_csv ('csv.northwind.order_details', 'file:../vad/order_details.csv', ',', '\n', null, 1, vector(1));
+ATTACH_FROM_CSV ('csv.northwind.order_details', 'file:../vad/order_details.csv', ',', '\n', null, 1, vector(1));
 SELECT TOP 5 * FROM csv.northwind.order_details;
 
 
 --ATTACH ORDERS TABLE
 
 DROP TABLE csv.northwind.orders;
-attach_from_csv ('csv.northwind.orders', 'file:../vad/orders.csv', ',', '\n', null, 1, vector(1));
+ATTACH_FROM_CSV ('csv.northwind.orders', 'file:../vad/orders.csv', ',', '\n', null, 1, vector(1));
 SELECT TOP 5 * FROM csv.northwind.orders;
 
 --ATTACH PRODUCTS TABLE
 
 DROP TABLE csv.northwind.products;
-attach_from_csv ('csv.northwind.products', 'file:../vad/products.csv', ',', '\n', null, 1, vector(1));
+ATTACH_FROM_CSV ('csv.northwind.products', 'file:../vad/products.csv', ',', '\n', null, 1, vector(1));
 SELECT TOP 5 * FROM csv.northwind.products;
 
 --ATTACH REGIONS TABLE
 
 DROP TABLE csv.northwind.regions;
-attach_from_csv ('csv.northwind.regions', 'file:../vad/regions.csv', ',', '\n', null, 1, vector(1));
+ATTACH_FROM_CSV ('csv.northwind.regions', 'file:../vad/regions.csv', ',', '\n', null, 1, vector(1));
 SELECT TOP 5 * FROM csv.northwind.regions;
 
 --ATTACH REGIONS TABLE
 
 DROP TABLE csv.northwind.regions;
-attach_from_csv ('csv.northwind.regions', 'file:../vad/regions.csv', ',', '\n', null, 1, vector(1));
+ATTACH_FROM_CSV ('csv.northwind.regions', 'file:../vad/regions.csv', ',', '\n', null, 1, vector(1));
 SELECT TOP 5 * FROM csv.northwind.regions;
 
 --ATTACH SHIPPERS TABLE
 
 DROP TABLE csv.northwind.shippers;
-attach_from_csv ('csv.northwind.shippers', 'file:../vad/shippers.csv', ',', '\n', null, 1, vector(1));
+ATTACH_FROM_CSV ('csv.northwind.shippers', 'file:../vad/shippers.csv', ',', '\n', null, 1, vector(1));
 SELECT TOP 5 * FROM csv.northwind.shippers;
 
 --ATTACH SUPPLIERS TABLE
 
 DROP TABLE csv.northwind.suppliers;
-attach_from_csv ('csv.northwind.suppliers', 'file:../vad/suppliers.csv', ',', '\n', null, 1, vector(1));
+ATTACH_FROM_CSV ('csv.northwind.suppliers', 'file:../vad/suppliers.csv', ',', '\n', null, 1, vector(1));
 SELECT TOP 5 * FROM csv.northwind.suppliers;
 
 
 --ATTACH TERRITORIES TABLE
 
 DROP TABLE csv.northwind.territories;
-attach_from_csv ('csv.northwind.territories', 'file:../vad/territories.csv', ',', '\n', null, 1, vector(1,2));
+ATTACH_FROM_CSV ('csv.northwind.territories', 'file:../vad/territories.csv', ',', '\n', null, 1, vector(1,2));
 SELECT TOP 5 * FROM csv.northwind.territories;
 
 
