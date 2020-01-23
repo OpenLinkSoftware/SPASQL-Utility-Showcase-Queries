c7b52699282dde54ba8b8ca1ece1a2b5954c89c8
OpenLink Software <github@openlinksw.com>
2020-01-23 16:00:25 -0500

    Blank nodes utility demonstration
    How Blank Nodes (indefinite pronouns) enable the use of RDF for powerful matching and recommendations.

---
+++ social-commerce/matching-person-to-products-from-company.sql
@@ -0,0 +1,53 @@
+-- Data Entry 
+-- Matching what a Person seeks (variant of interests) to a Software Application provided by an Organization. 
+
+SPARQL 
+
+CLEAR GRAPH <https://kidehen3.solid.openlinksw.com:8444/inbox/mydata.ttl> ;
+
+SPARQL 
+
+PREFIX schema: <http://schema.org/>
+PREFIX : <https://kidehen3.solid.openlinksw.com:8444/inbox/mydata.ttl#>
+PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+PREFIX virtuoso: <http://data.openlinksw.com/oplweb/product_family/virtuoso#> 
+PREFIX odbc-jdbc-bridge-st: <http://data.openlinksw.com/oplweb/product_release/UDASingleTierLiteEditionODBCRelease8JDBCBridge#> 
+
+INSERT INTO GRAPH <https://kidehen3.solid.openlinksw.com:8444/inbox/mydata.ttl> 
+{
+   [ a foaf:Person ;       
+       foaf:name "Kingsley Idehen"  
+    ] 
+    schema:seeks [ a schema:SoftwareApplication ; 
+                     schema:identifier odbc-jdbc-bridge-st:this ;                      
+                     schema:name "OpenLink ODBC to JDBC Bridge Driver" ;                        
+                     schema:description """A high-performance ODBC Driver for connecting to JDBC Data Sources. This Driver enables ODBC connectivity to existing ODBC-Compliant Applications from Data Sources that are JDBC-accessible. """@en ;                        
+                     schema:creator <http://www.openlinksw.com/#this>              
+                 ] ,
+                 [ a schema:SoftwareApplication ; 
+                     schema:identifier virtuoso:this ;                       
+                     schema:name "Virtuoso Universal Server" ;                        
+                     schema:description """A high-performance, secure, and scalable platform for developing, deploying, and exploiting personal and enterprise Knowledge Graphs. """@en ;                        
+                     schema:creator <http://www.openlinksw.com/#this>              
+                 ]  . 
+} ;
+
+-- Data Query 
+
+SPARQL
+
+PREFIX : <https://kidehen3.solid.openlinksw.com:8444/inbox/mydata.ttl#>
+
+SELECT DISTINCT ?uri ?type ?name ?desc 
+FROM <https://kidehen3.solid.openlinksw.com:8444/inbox/mydata.ttl> 
+WHERE { 
+        [ a foaf:Person ; 
+            foaf:name "Kingsley Idehen" 
+        ] 
+        schema:seeks [ a ?type ;                        
+                            schema:name ?name ;                        
+                            schema:description ?desc  ;   
+                            schema:identifier ?uri ;                
+                            schema:creator <http://www.openlinksw.com/#this>             
+                     ]  
+      } ;
