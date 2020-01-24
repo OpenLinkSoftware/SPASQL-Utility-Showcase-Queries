9f377eb878a24e40cc9a96edafd6f1203e2fd13f
OpenLink Software <github@openlinksw.com>
2020-01-24 16:42:09 -0500

    Update matching-person-to-products-from-company.sql

--- social-commerce/matching-person-to-products-from-company.sql
+++ social-commerce/matching-person-to-products-from-company.sql
@@ -19,13 +19,13 @@ INSERT INTO GRAPH <https://kidehen3.solid.openlinksw.com:8444/inbox/mydata.ttl>
        foaf:name "Kingsley Idehen"  
     ] 
     schema:seeks [ a schema:SoftwareApplication ; 
-                     schema:identifier odbc-jdbc-bridge-st:this ;                      
+                     schema:uri odbc-jdbc-bridge-st:this ;                      
                      schema:name "OpenLink ODBC to JDBC Bridge Driver" ;                        
                      schema:description """A high-performance ODBC Driver for connecting to JDBC Data Sources. This Driver enables ODBC connectivity to existing ODBC-Compliant Applications from Data Sources that are JDBC-accessible. """@en ;                        
                      schema:creator <http://www.openlinksw.com/#this>              
                  ] ,
                  [ a schema:SoftwareApplication ; 
-                     schema:identifier virtuoso:this ;                       
+                     schema:uri virtuoso:this ;                       
                      schema:name "Virtuoso Universal Server" ;                        
                      schema:description """A high-performance, secure, and scalable platform for developing, deploying, and exploiting personal and enterprise Knowledge Graphs. """@en ;                        
                      schema:creator <http://www.openlinksw.com/#this>              
@@ -47,7 +47,7 @@ WHERE {
         schema:seeks [ a ?type ;                        
                             schema:name ?name ;                        
                             schema:description ?desc  ;   
-                            schema:identifier ?uri ;                
+                            schema:uri ?uri ;                
                             schema:creator <http://www.openlinksw.com/#this>             
                      ]  
       } ;
