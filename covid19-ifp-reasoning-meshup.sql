1e8cb3f0808d5475205c0a87b0277509ad3f0487
OpenLink Software <github@openlinksw.com>
2020-04-15 10:49:59 -0400

    Additional mappings

--- covid19-ifp-reasoning-meshup.sql
+++ covid19-ifp-reasoning-meshup.sql
@@ -25,7 +25,10 @@ PREFIX sarscov2: <https://docs.google.com/spreadsheets/d/1Z0csxYkWFuvNpfaGQDKnhq
 PREFIX dbpedia-iso-mappings: <https://docs.google.com/spreadsheets/d/1Z0csxYkWFuvNpfaGQDKnhqdREHcwYbvd3s7vHsWsT5Q/gviz/tq?tqx=out:csv&sheet=DBpedia_Country_ISO_Code_Mappings#> 
 PREFIX us-covid-tracker-daily: <https://docs.google.com/spreadsheets/d/1Z0csxYkWFuvNpfaGQDKnhqdREHcwYbvd3s7vHsWsT5Q/gviz/tq?tqx=out:csv&sheet=US_State_Tracker_Daily#> 
 PREFIX us-covid-tracker-todate: <https://docs.google.com/spreadsheets/d/1Z0csxYkWFuvNpfaGQDKnhqdREHcwYbvd3s7vHsWsT5Q/gviz/tq?tqx=out:csv&sheet=US_State_Tracker_To_Date#> 
-
+PREFIX geocordinates: <https://opendata.arcgis.com/datasets/1b919b0ff5f44d05bcb374591206f757_0.csv?outSR=%7B%22latestWkid%22%3A3857%2C%22wkid%22%3A102100%7D#>
+PREFIX kaiseer-icus-by-us-county: <https://docs.google.com/spreadsheets/d/1Z0csxYkWFuvNpfaGQDKnhqdREHcwYbvd3s7vHsWsT5Q/gviz/tq?tqx=out:csv&range=A1:I3143&sheet=Kaiser_ICUs_By_US_County#>
+PREFIX wikidata: <http://www.wikidata.org/entity/> 
+PREFIX johhs-hopkins-daily-reports: <urn:johns-hopkins:covid19:daily:reports#> 
 INSERT { 
          GRAPH <urn:covid19:data:meshup> { 
                                             consolidated:id owl:equivalentProperty testing-2:dbpedia_country_id, nextstrain-csv:dbpedia_country_id .
@@ -39,6 +42,13 @@ INSERT {
                                                                       rdfs:subPropertyOf lod:ifp_like .
 
                                             consolidated:class owl:equivalentClass nextstrain-csv:class, testing-2:class .
+                                            geocordinates:Y owl:equivalentProperty geo:long .
+                                            geocordinates:X owl:equivalentProperty geo:lat .
+
+                                            wikidata:P882 a owl:InverseFunctionalProperty ;
+                                                          owl:equivalentProperty kaiseer-icus-by-us-county:fips . 
+
+                                            johhs-hopkins-daily-reports:FIPS a owl:InverseFunctionalProperty . 
 											                    }  
         };
 
@@ -66,6 +76,15 @@ PREFIX worldometer: <https://docs.google.com/spreadsheets/d/1Z0csxYkWFuvNpfaGQDK
 PREFIX consolidated: <https://docs.google.com/spreadsheets/d/1Z0csxYkWFuvNpfaGQDKnhqdREHcwYbvd3s7vHsWsT5Q/gviz/tq?tqx=out:csv&sheet=Consolidated#>
 PREFIX nextstrain-metadata-tsv: <https://github.com/nextstrain/ncov/raw/master/data/metadata.tsv#> 
 PREFIX wikipedia: <https://docs.google.com/spreadsheets/d/1Z0csxYkWFuvNpfaGQDKnhqdREHcwYbvd3s7vHsWsT5Q/gviz/tq?tqx=out:csv&sheet=Wikipedia_Testing_By_Country#>
+PREFIX kff:       <https://www.kff.org/wp-content/uploads/2020/03/overall_clean-csv-for-map.csv#> 
+PREFIX pomucky-csv: <https://onemocneni-aktualne.mzcr.cz/api/v1/covid-19/pomucky.csv#> 
+PREFIX nhs-ccg: <https://files.digital.nhs.uk/D6/BAA97E/NHS%20Pathways%20Covid-19%20data%202020-04-01.csv#>
+PREFIX nhs-ccg-2: <https://files.digital.nhs.uk/1F/5113E1/111%20Online%20Covid-19%20data_2020-04-01.csv#> 
+PREFIX nhs-ward: <https://github.com/britishredcrosssociety/covid-19-vulnerability/raw/master/data/ward_nearest_hospitals.csv#> 
+PREFIX nigeria-cdc: <https://docs.google.com/spreadsheets/d/1Z0csxYkWFuvNpfaGQDKnhqdREHcwYbvd3s7vHsWsT5Q/gviz/tq?tqx=out:csv&range=A1:H16&sheet=CDC_Nigeria#> 
+PREFIX johns-hopkins-daily: <urn:johns-hopkins:covid19:daily:reports#Combined_Key>
+
+
 
 INSERT INTO GRAPH <virtrdf-label> 
 
@@ -76,6 +95,13 @@ INSERT INTO GRAPH <virtrdf-label>
   consolidated:location rdfs:subPropertyOf  virtrdf:label  .
   nextstrain-metadata-tsv:strain rdfs:subPropertyOf  virtrdf:label  .
   wikipedia:Country_or_region rdfs:subPropertyOf  virtrdf:label  .
+  kff:State rdfs:subPropertyOf  virtrdf:label  .
+  pomucky-csv:kraj rdfs:subPropertyOf  virtrdf:label  .
+  nhs-ccg:CCGName rdfs:subPropertyOf  virtrdf:label  .
+  nhs-ccg-2:ccgname rdfs:subPropertyOf  virtrdf:label  .
+  nhs-ward:WD19CD rdfs:subPropertyOf  virtrdf:label  .
+  nigeria-cdc:States_Affected rdfs:subPropertyOf  virtrdf:label  .
+  johns-hopkins-daily: rdfs:subPropertyOf  virtrdf:label  .
 } ;
 
 rdfs_rule_set ('virtrdf-label', 'virtrdf-label');
@@ -89,6 +115,15 @@ PREFIX worldometer: <https://docs.google.com/spreadsheets/d/1Z0csxYkWFuvNpfaGQDK
 PREFIX consolidated: <https://docs.google.com/spreadsheets/d/1Z0csxYkWFuvNpfaGQDKnhqdREHcwYbvd3s7vHsWsT5Q/gviz/tq?tqx=out:csv&sheet=Consolidated#>
 PREFIX nextstrain-metadata-tsv: <https://github.com/nextstrain/ncov/raw/master/data/metadata.tsv#> 
 PREFIX wikipedia: <https://docs.google.com/spreadsheets/d/1Z0csxYkWFuvNpfaGQDKnhqdREHcwYbvd3s7vHsWsT5Q/gviz/tq?tqx=out:csv&sheet=Wikipedia_Testing_By_Country#>
+PREFIX kff:       <https://www.kff.org/wp-content/uploads/2020/03/overall_clean-csv-for-map.csv#> 
+PREFIX pomucky-csv: <https://onemocneni-aktualne.mzcr.cz/api/v1/covid-19/pomucky.csv#> 
+PREFIX nhs-ccg: <https://files.digital.nhs.uk/D6/BAA97E/NHS%20Pathways%20Covid-19%20data%202020-04-01.csv#>
+PREFIX nhs-ccg-2: <https://files.digital.nhs.uk/1F/5113E1/111%20Online%20Covid-19%20data_2020-04-01.csv#> 
+PREFIX nhs-ward: <https://github.com/britishredcrosssociety/covid-19-vulnerability/raw/master/data/ward_nearest_hospitals.csv#> 
+PREFIX nigeria-cdc: <https://docs.google.com/spreadsheets/d/1Z0csxYkWFuvNpfaGQDKnhqdREHcwYbvd3s7vHsWsT5Q/gviz/tq?tqx=out:csv&range=A1:H16&sheet=CDC_Nigeria#> 
+PREFIX johns-hopkins-daily: <urn:johns-hopkins:covid19:daily:reports#Combined_Key>
+
+
 
 INSERT INTO GRAPH <facets> 
 
@@ -99,6 +134,13 @@ INSERT INTO GRAPH <facets>
     consolidated:location rdfs:subPropertyOf  virtrdf:label  .
     nextstrain-metadata-tsv:strain rdfs:subPropertyOf  virtrdf:label  .
     wikipedia:Country_or_region rdfs:subPropertyOf  virtrdf:label  .
+    kff:State rdfs:subPropertyOf  virtrdf:label  .
+    pomucky-csv:kraj rdfs:subPropertyOf  virtrdf:label  .
+    nhs-ccg:CCGName rdfs:subPropertyOf  virtrdf:label  .
+    nhs-ccg-2:ccgname rdfs:subPropertyOf  virtrdf:label  .
+    nhs-ward:WD19CD rdfs:subPropertyOf  virtrdf:label  .
+    nigeria-cdc:States_Affected rdfs:subPropertyOf  virtrdf:label  .
+    johns-hopkins-daily: rdfs:subPropertyOf  virtrdf:label  .
   } ;
 
 --Making the rule from theGRAPH:
@@ -112,6 +154,57 @@ rdfs_rule_set ('facets', 'virtrdf-label');
 
 SELECT rs_name FROM sys_rdf_schema
 WHERE rs_name = 'facets' OR rs_name = 'virtrdf-label' ;
+
+-- Ontology Label Fixes from CSV Transformations
+
+SPARQL
+
+PREFIX nhs-covid-tracing: <https://files.digital.nhs.uk/D6/BAA97E/NHS%20Pathways%20Covid-19%20data%202020-04-01.csv#>
+PREFIX nhs-covid-tracing-doc: <https://linkeddata.uriburner.com/about/id/https/files.digital.nhs.uk/D6/BAA97E/NHS%20Pathways%20Covid-19%20data%202020-04-01.>
+PREFIX nhs-covid-tracing-2: <https://files.digital.nhs.uk/1F/5113E1/111%20Online%20Covid-19%20data_2020-04-01.csv#>
+PREFIX nhs-covid-tracing-doc-2: <https://linkeddata.uriburner.com/about/id/https/files.digital.nhs.uk/1F/5113E1/111%20Online%20Covid-19%20data_2020-04-01.>
+PREFIX nhs-ward: <https://github.com/britishredcrosssociety/covid-19-vulnerability/raw/master/data/ward_nearest_hospitals.csv#> 
+PREFIX nigeria-cdc: <https://docs.google.com/spreadsheets/d/1Z0csxYkWFuvNpfaGQDKnhqdREHcwYbvd3s7vHsWsT5Q/gviz/tq?tqx=out:csv&range=A1:H16&sheet=CDC_Nigeria#> 
+PREFIX johns-hopkins-daily: <urn:johns-hopkins:covid19:daily:reports#>
+PREFIX johhs-hopkins-daily-reports: <urn:johns-hopkins:covid19:daily:reports#> 
+
+
+
+
+INSERT INTO GRAPH <urn:kidehen:covid19:label:fixes> 
+
+  {
+    nhs-covid-tracing:ontology skos:prefLabel  "UK NHS COVID-19 Pathways Ontology"  ;
+                               rdfs:comment """Definition of terms used to define COVID-19 Disease Tracking, across annonymized subjects""" . 
+    nhs-covid-tracing:class skos:prefLabel  "Disease Tracking Collection"  ;
+                               rdfs:comment """Class to which disease tracking events belong""" . 
+    nhs-covid-tracing-doc:csv  skos:prefLabel  "NHS Disease Tracking Collection Document"  ;
+                               rdfs:comment """RDF sentence collection generated from CSV documents about COVID19 
+                                               Disease Tracking by the UK NHS""" . 
+    nhs-covid-tracing-2:ontology skos:prefLabel  "UK NHS COVID-19 Disease Spread Potential Ontology"  ;
+                               rdfs:comment """Definition of terms used to define COVID-19 Disease Spread potential, across annonymized subjects""" . 
+    nhs-covid-tracing-2:class skos:prefLabel  "Disease Spread Potential Tracking Collection"  ;
+                               rdfs:comment """Class of events for tracking disease spread potential""" .   
+    nhs-covid-tracing-doc-2:csv  skos:prefLabel  "NHS Disease Spread Potential Tracking Collection Document"  ;
+                               rdfs:comment """RDF sentence collection generated from CSV documents about COVID19 
+                                               Disease Spread Potential Tracking by the UK NHS""" .        
+
+    nigeria-cdc:ontology skos:prefLabel  "Nigeria CDC COVID-19 Data Ontology"  ;
+                               rdfs:comment """Definition of terms used to define Nigeria CDC COVID-19 Data""" . 
+    nigeria-cdc:class skos:prefLabel  "Nigeria CDC State Data Collection"  ;
+                               rdfs:comment """Categorization of Data for a given State""" .   
+    nigeria-cdc:csv  skos:prefLabel  "About CDC Nigeria COVID-19 Data by State"  ;
+                               rdfs:comment """RDF sentence collection generated from CSV documents about Nigeria CDC COVID-19 Data by State.""" .  
+
+    johns-hopkins-daily:ontology  skos:prefLabel  "Johns Hopkins Daily COVID-19 CSV Dumps Ontology"  ;
+                                  rdfs:comment """Definition of terms used to define Johns's Hopkins Daily Data from CSV Documents.""" .       
+    johns-hopkins-daily:class     skos:prefLabel  "Johns Hopkins Daily COVID-19 CSV Data Collection"  ;
+                                  rdfs:comment """Categorization of COVID-19 Data for Johns Hopkins Daily""" .    
+
+    johhs-hopkins-daily-reports:dbpedia_country_id  skos:prefLabel  "DBpedia Country ID" .
+    johhs-hopkins-daily-reports:dbpedia_state_id    skos:prefLabel  "DBpedia State ID" .  
+    johhs-hopkins-daily-reports:dbpedia_county_id   skos:prefLabel  "DBpedia County ID" .
+  } ;
 
 --  Test 1
 
