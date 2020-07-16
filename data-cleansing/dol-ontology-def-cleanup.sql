fd7ef4a59b489dcd04a1457b3d22027ecb88c318
OpenLink Software <github@openlinksw.com>
2020-07-16 15:25:12 -0400

    Update dol-ontology-def-cleanup.sql

--- data-cleansing/dol-ontology-def-cleanup.sql
+++ data-cleansing/dol-ontology-def-cleanup.sql
@@ -6,6 +6,9 @@ PREFIX ontology: <http://www.omg.org/spec/DOL/DOL-terms/>
 PREFIX ontology-doc: <https://www.omg.org/spec/DOL/20171106/DOL-terms.rdf> 
 PREFIX vocab: <http://open.vocab.org/terms/>
 PREFIX wdrs: <http://www.w3.org/2007/05/powder-s#>
+PREFIX owl: <http://www.w3.org/2002/07/owl#>
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> 
 
 INSERT INTO <urn:uriburner:ontology:fixes> 
 {
