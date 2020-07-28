032f02ae4d7ec04685a7c86a256a10881b5d6d16
danielhmills <5824561+danielhmills@users.noreply.github.com>
2020-07-28 18:46:02 -0400

    Added Wikidata and KBpedia prefixes + SPARQL-FED functionality

---
+++ reasoning-and-inference/built-in-reasoning-and-inference-kb-philosopher.sql
@@ -0,0 +1,214 @@
+SPARQL CLEAR GRAPH <urn:qa:ifr:kbpedia:philosopher:tbox>;
+SPARQL CLEAR GRAPH <urn:qa:ifr:wikidata:philosopher:tbox>;
+SPARQL CLEAR GRAPH <urn:qa:ifr:wikidata:philosopher:abox>;
+
+--KBpedia Tbox (Terminology Box)
+
+SPARQL
+PREFIX wd: <http://www.wikidata.org/entity/>
+PREFIX wdt: <http://www.wikidata.org/prop/direct/>
+PREFIX owl: <http://www.w3.org/2002/07/owl#>
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+PREFIX kbpedia: <http://kbpedia.org/kko/rc/>
+
+INSERT INTO GRAPH <urn:qa:ifr:kbpedia:philosopher:tbox>
+{
+    ?class a ?o
+}
+
+WHERE
+    {
+        SERVICE <https://wikidata.demo.openlinksw.com/sparql>
+        {
+            SELECT *
+            FROM <https://github.com/Cognonto/kbpedia/raw/master/versions/2.50/kbpedia_reference_concepts.zip>
+            WHERE
+                {
+                    ?class a ?o.
+                    FILTER (?class = kbpedia:Philosopher).
+                }
+        }
+        
+    };
+
+-- Wikidata Tbox (Terminology Box)
+
+SPARQL
+PREFIX wd: <http://www.wikidata.org/entity/>
+PREFIX wdt: <http://www.wikidata.org/prop/direct/>
+PREFIX owl: <http://www.w3.org/2002/07/owl#>
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+PREFIX kbpedia: <http://kbpedia.org/kko/rc/>
+
+INSERT INTO GRAPH <urn:qa:ifr:wikidata:philosopher:tbox>
+{
+    ?class a ?o
+}
+
+WHERE
+    {
+        SERVICE <https://wikidata.demo.openlinksw.com/sparql>
+        {
+            SELECT *
+            #FROM <https://github.com/Cognonto/kbpedia/raw/master/versions/2.50/kbpedia_reference_concepts.zip>
+            WHERE
+                {
+                    ?class a ?o.
+                    FILTER (?class = wd:Q4964182).
+                }
+        }
+        
+    };
+
+-- Wikidata Abox (Assertions Box)
+
+SPARQL
+PREFIX wd: <http://www.wikidata.org/entity/>
+PREFIX wdt: <http://www.wikidata.org/prop/direct/>
+PREFIX owl: <http://www.w3.org/2002/07/owl#>
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+PREFIX kbpedia: <http://kbpedia.org/kko/rc/>
+
+INSERT INTO GRAPH <urn:qa:ifr:wikidata:philosopher:abox>
+{
+    ?entity ?p ?class
+}
+
+WHERE
+    {
+        SERVICE <https://wikidata.demo.openlinksw.com/sparql>
+        {
+            SELECT *
+            WHERE
+                {
+                    ?entity ?p ?class.
+                    FILTER (?p = wdt:P106 && ?class = wd:Q4964182).
+                }
+        }
+        
+    };
+
+SPARQL 
+CLEAR  GRAPH <https://github.com/Cognonto/kbpedia/raw/master/versions/2.50/kbpedia_reference_concepts.zip> ;
+DELETE
+FROM DB.DBA.SYS_RDF_SCHEMA
+WHERE RS_NAME = 'urn:kbpedia:philosopher:owl:equivalent:class:subproperty:inference:rules' ;
+
+SPARQL
+PREFIX wd: <http://www.wikidata.org/entity/>
+PREFIX kbpedia: <http://kbpedia.org/kko/rc/>
+
+DELETE {
+         GRAPH <https://github.com/Cognonto/kbpedia/raw/master/versions/2.50/kbpedia_reference_concepts.zip> 
+            {
+              wd:Q4964182 owl:equivalentClass kbpedia:Philosopher .
+
+            }
+        } ;
+
+SPARQL
+PREFIX wd: <http://www.wikidata.org/entity/>
+PREFIX wdt: <http://www.wikidata.org/prop/direct/>
+PREFIX owl: <http://www.w3.org/2002/07/owl#>
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+PREFIX kbpedia: <http://kbpedia.org/kko/rc/>
+
+INSERT {
+         GRAPH <https://github.com/Cognonto/kbpedia/raw/master/versions/2.50/kbpedia_reference_concepts.zip> 
+            {
+                kbpedia:Philosopher owl:equivalentClass wd:Q4964182 .
+               #wd:Q4964182 owl:equivalentClass kbpedia:Philosopher .
+                wdt:P106 rdfs:subPropertyOf rdf:type .
+                owl:equivalentProperty a owl:TransitiveProperty . 
+                owl:equivalentClass a owl:TransitiveProperty.  
+                rdfs:subPropertyOf a owl:TransitiveProperty .
+                rdfs:subClassOf a owl:TransitiveProperty . 
+            }
+       } ;
+
+RDFS_RULE_SET ('urn:kbpedia:philosopher:owl:equivalent:class:subproperty:inference:rules', 'https://github.com/Cognonto/kbpedia/raw/master/versions/2.50/kbpedia_reference_concepts.zip') ;
+
+SELECT *
+FROM DB.DBA.SYS_RDF_SCHEMA
+WHERE RS_NAME = 'urn:kbpedia:philosopher:owl:equivalent:class:subproperty:inference:rules' ;
+
+-- Tests
+
+-- 1.1 Test using class kbpedia:Philosopher
+
+
+SPARQL
+DEFINE input:inference 'urn:kbpedia:philosopher:owl:equivalent:class:subproperty:inference:rules'
+
+PREFIX wd: <http://www.wikidata.org/entity/>
+PREFIX wdt: <http://www.wikidata.org/prop/direct/>
+PREFIX owl: <http://www.w3.org/2002/07/owl#>
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+PREFIX bmo: <http://purl.org/bmo/ns#>
+PREFIX schema: <http://schema.org/>
+PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+PREFIX kbpedia: <http://kbpedia.org/kko/rc/>
+
+
+SELECT COUNT(*)
+WHERE {
+                ?s a kbpedia:Philosopher .
+          } ;
+
+
+-- Test 1.2: without reasoning
+
+SPARQL
+# DEFINE input:inference 'urn:kbpedia:philosopher:owl:equivalent:class:subproperty:inference:rules'
+PREFIX wd: <http://www.wikidata.org/entity/>
+PREFIX wdt: <http://www.wikidata.org/prop/direct/>
+PREFIX owl: <http://www.w3.org/2002/07/owl#>
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+PREFIX bmo: <http://purl.org/bmo/ns#>
+PREFIX schema: <http://schema.org/>
+PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+PREFIX kbpedia: <http://kbpedia.org/kko/rc/>
+
+SELECT COUNT(*)
+WHERE {
+                ?s a kbpedia:Philosopher .
+          } ;
+
+-- Tests
+
+-- 2.1 Test using class wdt:Q4964182
+
+SPARQL
+DEFINE input:inference 'urn:kbpedia:philosopher:owl:equivalent:class:subproperty:inference:rules'
+
+PREFIX wd: <http://www.wikidata.org/entity/>
+PREFIX wdt: <http://www.wikidata.org/prop/direct/>
+PREFIX owl: <http://www.w3.org/2002/07/owl#>
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+PREFIX bmo: <http://purl.org/bmo/ns#>
+PREFIX schema: <http://schema.org/>
+PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+PREFIX kbpedia: <http://kbpedia.org/kko/rc/>
+
+
+SELECT COUNT(*)
+WHERE {
+                ?s a wd:Q4964182 .
+          } ;
+
+-- Test 2.2: without reasoning
+SPARQL
+# DEFINE input:inference 'urn:kbpedia:philosopher:owl:equivalent:class:subproperty:inference:rules'
+PREFIX wd: <http://www.wikidata.org/entity/>
+PREFIX wdt: <http://www.wikidata.org/prop/direct/>
+PREFIX owl: <http://www.w3.org/2002/07/owl#>
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+PREFIX bmo: <http://purl.org/bmo/ns#>
+PREFIX schema: <http://schema.org/>
+PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+PREFIX kbpedia: <http://kbpedia.org/kko/rc/>
+
+SELECT COUNT(*)
+WHERE {
+                ?s a wd:Q4964182 .
+          } ;
---
+++ reasoning-and-inference/built-in-reasoning-and-inference-kb-stage-actor.sql
@@ -0,0 +1,183 @@
+SPARQL CLEAR GRAPH <urn:qa:ifr:kbpedia:stage-actor:tbox>;
+SPARQL CLEAR GRAPH <urn:qa:ifr:wikidata:stage-actor:tbox>;
+SPARQL CLEAR GRAPH <urn:qa:ifr:wikidata:stage-actor:abox>;
+
+--KBpedia Tbox (Terminology Box)
+
+
+SPARQL
+
+PREFIX wdt: <http://www.wikidata.org/entity/>
+PREFIX wd: <http://www.wikidata.org/prop/direct/>
+PREFIX kbpedia: <http://kbpedia.org/kko/rc/>
+
+INSERT INTO GRAPH <urn:qa:ifr:kbpedia:stage-actor:tbox>
+{
+    ?class a ?o
+}
+
+WHERE
+    {
+        SERVICE <https://wikidata.demo.openlinksw.com/sparql>
+        {
+            SELECT *
+            #FROM <https://github.com/Cognonto/kbpedia/raw/master/versions/2.50/kbpedia_reference_concepts.zip>
+            WHERE
+                {
+                    ?class a ?o.
+                    FILTER (?class = kbpedia:ActorInPlays).
+                }
+        }
+        
+    };
+
+-- Wikidata Tbox (Terminology Box)
+
+SPARQL
+
+PREFIX wd: <http://www.wikidata.org/entity/>
+PREFIX wdt: <http://www.wikidata.org/prop/direct/>
+PREFIX kbpedia: <http://kbpedia.org/kko/rc/>
+INSERT INTO GRAPH <urn:qa:ifr:wikidata:stage-actor:tbox>
+{
+    ?class a ?o
+}
+
+WHERE
+    {
+        SERVICE <https://wikidata.demo.openlinksw.com/sparql>
+        {
+            SELECT *
+            #FROM <https://github.com/Cognonto/kbpedia/raw/master/versions/2.50/kbpedia_reference_concepts.zip>
+            WHERE
+                {
+                    ?class a ?o.
+                    FILTER (?class = wd:Q2259451).
+                }
+        }
+        
+    };
+
+-- Wikidata Abox (Assertions Box)
+
+SPARQL
+PREFIX wd: <http://www.wikidata.org/entity/>
+PREFIX wdt: <http://www.wikidata.org/prop/direct/>
+PREFIX kbpedia: <http://kbpedia.org/kko/rc/>
+INSERT INTO GRAPH <urn:qa:ifr:wikidata:stage-actor:abox>
+{
+    ?entity ?p ?class
+}
+
+WHERE
+    {
+        SERVICE <https://wikidata.demo.openlinksw.com/sparql>
+        {
+            SELECT *
+            WHERE
+                {
+                    ?entity ?p ?class.
+                    FILTER (?p = wdt:P106 && ?class = wd:Q2259451).
+                }
+            LIMIT 500
+        }
+        
+    };
+
+-- Built-in inference rules
+
+SPARQL 
+CLEAR  GRAPH <https://github.com/Cognonto/kbpedia/raw/master/versions/2.50/kbpedia_reference_concepts.zip> ;
+DELETE
+FROM DB.DBA.SYS_RDF_SCHEMA
+WHERE RS_NAME = 'urn:kbpedia:stage-actor:owl:equivalent:class:subproperty:inference:rules' ;
+
+SPARQL
+PREFIX wd: <http://www.wikidata.org/entity/>
+PREFIX wdt: <http://www.wikidata.org/prop/direct/>
+PREFIX owl: <http://www.w3.org/2002/07/owl#>
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+PREFIX kbpedia: <http://kbpedia.org/kko/rc/>
+DELETE {
+         GRAPH <https://github.com/Cognonto/kbpedia/raw/master/versions/2.50/kbpedia_reference_concepts.zip> 
+            {
+              #kbpedia:ActorInPlays owl:equivalentClass wd:Q2259451 .
+              wd:Q2259451 owl:equivalentClass kbpedia:ActorInPlays .
+            }
+        } ;
+
+SPARQL
+PREFIX wd: <http://www.wikidata.org/entity/>
+PREFIX wdt: <http://www.wikidata.org/prop/direct/>
+PREFIX owl: <http://www.w3.org/2002/07/owl#>
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+PREFIX kbpedia: <http://kbpedia.org/kko/rc/>
+INSERT {
+         GRAPH <https://github.com/Cognonto/kbpedia/raw/master/versions/2.50/kbpedia_reference_concepts.zip> 
+            {
+                kbpedia:ActorInPlays owl:equivalentClass wd:Q2259451 .
+                wdt:P106 rdfs:subPropertyOf rdf:type .
+                owl:equivalentProperty a owl:TransitiveProperty . 
+                owl:equivalentClass a owl:TransitiveProperty.  
+                rdfs:subPropertyOf a owl:TransitiveProperty .
+                rdfs:subClassOf a owl:TransitiveProperty . 
+            }
+       } ;
+
+RDFS_RULE_SET ('urn:kbpedia:stage-actor:owl:equivalent:class:subproperty:inference:rules', 'https://github.com/Cognonto/kbpedia/raw/master/versions/2.50/kbpedia_reference_concepts.zip') ;
+
+SELECT *
+FROM DB.DBA.SYS_RDF_SCHEMA
+WHERE RS_NAME = 'urn:kbpedia:stage-actor:owl:equivalent:class:subproperty:inference:rules' ;
+
+-- Tests
+
+-- 1.1 Test using class kbpedia:ActorInPlays
+
+
+SPARQL
+DEFINE input:inference 'urn:kbpedia:stage-actor:owl:equivalent:class:subproperty:inference:rules'
+PREFIX kbpedia: <http://kbpedia.org/kko/rc/>
+
+
+SELECT COUNT(*)
+WHERE {
+                ?s a kbpedia:ActorInPlays .
+          } ;
+
+
+-- Test 1.2: without reasoning
+
+SPARQL
+# DEFINE input:inference 'urn:kbpedia:stage-actor:owl:equivalent:class:subproperty:inference:rules'
+PREFIX kbpedia: <http://kbpedia.org/kko/rc/>
+
+SELECT COUNT(*)
+WHERE {
+                ?s a kbpedia:ActorInPlays .
+          } ;
+
+-- Tests
+
+-- 2.1 Test using class wdt:Q2259451
+
+SPARQL
+DEFINE input:inference 'urn:kbpedia:stage-actor:owl:equivalent:class:subproperty:inference:rules'
+PREFIX wd: <http://www.wikidata.org/entity/>
+
+
+
+SELECT COUNT(*)
+WHERE {
+                ?s a wd:Q2259451 .
+          } ;
+
+-- Test 2.2: without reasoning
+SPARQL
+# DEFINE input:inference 'urn:kbpedia:stage-actor:owl:equivalent:class:subproperty:inference:rules'
+PREFIX wd: <http://www.wikidata.org/entity/>
+
+SELECT COUNT(*)
+WHERE {
+                ?s a wd:Q2259451 .
+          } ;
