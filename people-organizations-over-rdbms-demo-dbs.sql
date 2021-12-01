85ffdffc2f66d9b007b44f23a898e8baa5080151
Kingsley Idehen <kidehen@openlinksw.com>
2021-12-01 17:17:20 -0500

    Added MySQL and PostgreSQL Tables to the mix

--- people-organizations-over-rdbms-demo-dbs.sql
+++ people-organizations-over-rdbms-demo-dbs.sql
@@ -22,71 +22,40 @@ INSERT
       GRAPH <urn:rdbms:inference:reasoning:demo:1>
          {
             ### Organization Class Instances (i.e. Organizations) ###
-	    
-	    ## Class derived from "Demo"."demo"."Customers" Table
-            <http://demo.openlinksw.com/schemas/Demo/Customers>
-               rdfs:subClassOf   foaf:Organization   .
 
-            ## Class derived from "sqlserver"."northwind"."Customers" Table
+            ## Class derived from "mssql"."northwind"."customers" Table
             <http://demo.openlinksw.com/schemas/SQLServer/Customers>
                rdfs:subClassOf   foaf:Organization   .
-
-            ## Class derived from "sybase12."pubs2"."publishers" Table
-            <http://demo.openlinksw.com/schemas/sybase12/publishers>
-               rdfs:subClassOf   foaf:Organization   .
-
-            ## Class derived from "progress11"."pub"."Customer" Table
-	    <http://demo.openlinksw.com/schemas/progress11/Customer>
-               rdfs:subClassOf   foaf:Organization   .
-	       
-	    # Class derived from "progress11"."pub"."Supplier" Table   
-	     <http://demo.openlinksw.com/schemas/progress11/Supplier>
-	        rdfs:subClassOf   foaf:Organization   .
-
-            ## Class derived from "PrestoDB"."hive"."customer" Table
-            <http://demo.openlinksw.com/schemas/PrestoDB/customer>
-               rdfs:subClassOf   foaf:Organization   .
 	    
-	    ## Class derived from "csv"."demo"."companies" Table
-	    <http://demo.openlinksw.com/schemas/csv/companies> 
-	        rdfs:subClassOf   foaf:Organization   .
-	    
-	    ## Class derived from "SQLSerever"."Northwind"."Customers" Table
-	    <http://demo.openlinksw.com/schemas/SQLServer/Customers>
-	        rdfs:subClassOf   foaf:Organization   .
+	    ## Class derived from "csv"."northwind"."customers" Table
+	    <http://demo.openlinksw.com/schemas/csv/customers> 
+	       rdfs:subClassOf   foaf:Organization   .
+
+            ## Class derived from "postgres12"."northwind"."suppliers" Table
+            <http://demo.openlinksw.com/schemas/postgres12/suppliers>  
+            rdfs:subClassOf   foaf:Organization   .
+
+            ## Class derived from "mysql5"."northwind"."suppliers" Table
+            <http://demo.openlinksw.com/schemas/mysql5/suppliers>  
+            rdfs:subClassOf   foaf:Organization   .
 	       
             ### Person Class Instances (i.e. People) ###
-	    
-	    ## Class derived from "progress11"."pubs"."Employee" Table
-	    <http://demo.openlinksw.com/schemas/progress11/Employee>
-	       rdfs:subClassOf   foaf:Person   .
-	    
-	    # Class derived from "progress11"."pubs"."Salesrep" Table  
-	    <http://demo.openlinksw.com/schemas/progress11/Salesrep>
-	       rdfs:subClassOf   foaf:Person   .
-		
-	    ## Class derived from Virtuoso "School"."DBA"."Person" Table
-            <http://demo.openlinksw.com/schemas/School/Person>
-	       rdfs:subClassOf   foaf:Person   .
-	       
-            ## Class derived from "informix"."stores"."customer" Table
+            
+            ## Class derived from "hr"."ora18"."EMPLOYEES" Table
+            <http://demo.openlinksw.com/schemas/OracleHR/EMPLOYEES>
+               rdfs:subClassOf   foaf:Person   .
+           
+           ## Class derived from "informix"."stores"."customer" Table
             <http://demo.openlinksw.com/schemas/informix/customer>
                rdfs:subClassOf   foaf:Person   .
 
-            ## Class derived from "ingres"."tutorial"."cust_Info" Table
-            <http://demo.openlinksw.com/schemas/ingres/cust_info>
-               rdfs:subClassOf   foaf:Person   .
+           ## Class derived from Virtuoso "School"."DBA"."Person" Table
+                <http://demo.openlinksw.com/schemas/School/Person>
+            rdfs:subClassOf   foaf:Person   .
+	      
 
-            ## Class derived from "Oracle"."HR"."EMPLOYEES" Table
-            <http://demo.openlinksw.com/schemas/OracleHR/EMPLOYEES>
-               rdfs:subClassOf   foaf:Person   .
-	       
-	    ## Class derived from "SQLServer"."Northwind"."EMPLOYEES" Table
-            <http://demo.openlinksw.com/schemas/SQLServer/Employees>
-               rdfs:subClassOf   foaf:Person   .
          }
    } ;
- 
 
 -- Ensure that terms from the RDF Schema Vocabulary 
 -- are also loaded to the Named Graph identified by <urn:rdbms:inference:reasoning:demo:1> .
@@ -124,14 +93,7 @@ WHERE
              bind (concat(?fname,' ', ?sname) as ?name)
          }
       }
-  
-   UNION
-  
-      {  GRAPH <urn:demo.openlinksw.com:ingres>
-         {
-             ?s   <http://demo.openlinksw.com/schemas/ingres/name>   ?name   .
-         }
-      }
+
   
    UNION
   
@@ -163,17 +125,17 @@ WHERE
   
    UNION
 
-      {  GRAPH <urn:demo.openlinksw.com:progress9>
+      {  GRAPH <urn:demo.openlinksw.com:mysql5>
          {
-             ?s   <http://demo.openlinksw.com/schemas/progress9/name>   ?name   .
+             ?s   <http://demo.openlinksw.com/schemas/mysql5/firstname>   ?name   .
          }
       }
   
    UNION
   
-      {  GRAPH <urn:demo.openlinksw.com:sybase12>
+      {  GRAPH <urn:demo.openlinksw.com:postgres12>
          {
-            ?s   <http://demo.openlinksw.com/schemas/sybase12/pub_name>   ?name   .
+            ?s   <http://demo.openlinksw.com/schemas/postgres12/first_name>   ?name   .
          }
       }
 
@@ -237,7 +199,7 @@ FROM <urn:demo.openlinksw.com:School>
 FROM <urn:demo.openlinksw.com:progress11>
 FROM <urn:demo.openlinksw.com:SQLServer> 
 FROM <urn:demo.openlinksw.com:sybase12> 
-FROM <urn:demo.openlinksw.com:prestoDB> 
+FROM <urn:demo.openlinksw.com:prestoDB>
 WHERE {
         ?s a foaf:Person .
       }  
@@ -258,6 +220,8 @@ FROM <urn:demo.openlinksw.com:SQLServer>
 FROM <urn:demo.openlinksw.com:School> 
 FROM <urn:demo.openlinksw.com:sybase12> 
 FROM <urn:demo.openlinksw.com:prestoDB> 
+FROM <urn:demo.openlinksw.com:postgres12>
+FROM <urn:demo.openlinksw.com:mysql5> 
 WHERE {
         ?s a foaf:Organization .
       }  
@@ -278,6 +242,8 @@ FROM <urn:demo.openlinksw.com:SQLServer>
 FROM  <urn:demo.openlinksw.com:School>  
 FROM <urn:demo.openlinksw.com:sybase12> 
 FROM <urn:demo.openlinksw.com:prestoDB> 
+FROM <urn:demo.openlinksw.com:postgres12>
+FROM <urn:demo.openlinksw.com:mysql5> 
 WHERE {
         ?s a foaf:Organization .
       }  
@@ -299,6 +265,8 @@ FROM NAMED <urn:demo.openlinksw.com:School>
 FROM NAMED <urn:demo.openlinksw.com:sybase12> 
 FROM NAMED <urn:demo.openlinksw.com:prestoDB> 
 FROM NAMED <urn:demo.openlinksw.com:csv> 
+FROM <urn:demo.openlinksw.com:postgres12>
+FROM <urn:demo.openlinksw.com:mysql5> 
 WHERE {
         { 
          SELECT DISTINCT ?s { GRAPH <urn:demo.openlinksw.com:OracleHR> {?s a foaf:Organization . } } LIMIT 1
@@ -372,6 +340,8 @@ FROM NAMED <urn:demo.openlinksw.com:School>
 FROM NAMED <urn:demo.openlinksw.com:sybase12> 
 FROM NAMED <urn:demo.openlinksw.com:prestoDB> 
 FROM NAMED <urn:demo.openlinksw.com:csv> 
+FROM <urn:demo.openlinksw.com:postgres12>
+FROM <urn:demo.openlinksw.com:mysql5> 
 WHERE {
         { 
          SELECT DISTINCT ?s { GRAPH <urn:demo.openlinksw.com:OracleHR> {?s a foaf:Organization . } } LIMIT 1
@@ -444,6 +414,8 @@ FROM NAMED <urn:demo.openlinksw.com:School>
 FROM NAMED <urn:demo.openlinksw.com:sybase12> 
 FROM NAMED <urn:demo.openlinksw.com:prestoDB> 
 FROM NAMED <urn:demo.openlinksw.com:csv> 
+FROM <urn:demo.openlinksw.com:postgres12>
+FROM <urn:demo.openlinksw.com:mysql5> 
 WHERE {
         { 
          SELECT DISTINCT ?s { GRAPH <urn:demo.openlinksw.com:OracleHR> {?s a foaf:Person .  } } LIMIT 1
@@ -517,6 +489,8 @@ FROM NAMED <urn:demo.openlinksw.com:School>
 FROM NAMED <urn:demo.openlinksw.com:sybase12> 
 FROM NAMED <urn:demo.openlinksw.com:prestoDB> 
 FROM NAMED <urn:demo.openlinksw.com:csv> 
+FROM <urn:demo.openlinksw.com:postgres12>
+FROM <urn:demo.openlinksw.com:mysql5> 
 WHERE {
         { 
          SELECT DISTINCT ?s { GRAPH <urn:demo.openlinksw.com:OracleHR> {?s a foaf:Person .  } } LIMIT 1
